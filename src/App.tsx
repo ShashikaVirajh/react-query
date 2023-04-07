df524bbdf841e15fcc68806e1d1d70138bc37bab
Shashika Virajh <shashikasvka@gmail.com>
2023-04-07 17:38:34 +0530

    Initialized project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Typography } from '@mui/material';
+import { FC } from 'react';
 
-function App() {
+export const App: FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container justifyContent='center' spacing={'1rem'}>
+      <Typography>React Query Example</Typography>
+    </Grid>
   );
-}
-
-export default App;
+};
